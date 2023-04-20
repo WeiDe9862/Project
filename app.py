c722c1e2165de3d491d98e3e594f1cb7b856574c
WeiDe9862 <feed9863@gmail.com>
2023-04-20 16:18:47 +0800

    登入正確、錯誤

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ def login():
     if request.method =="POST":
         name=request.form.get("Account")
         password = request.form.get("Password")
-        return render_template("page2.html",id=name,ps=password)
+        if name=="admin" and password=="1234":
+            type="登入成功"
+            return render_template("page2.html",id=name,ps=password,type=type)
+        else:
+            type="登入失敗"
+            return render_template('login.html',id=name,ps=password,type=type)
     else:
         return render_template('login.html')
 
